164d95f672e4febdf6ea72d26448132239114891
Arthur K <arthurk031@outlook.com>
2025-01-10 15:37:50 -0300

    Início

---
+++ BillingMVC.IOC/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BillingMVC.IOC
+{
+    public static class DependencyInjection
+    {
+        public static void InjectServices(this IServiceCollection services)
+        {
+
+        }
+
+        public static void InjectRepositories(this IServiceCollection services)
+        {
+
+        }
+    }
+}
