1378a3ec1b2508afdf95483d9e050fb336be3fc7
Arthur K <arthurk031@outlook.com>
2025-01-10 15:46:43 -0300

    Novas pastas

---
+++ BillingMVC.Data/Configurations/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Arquivo adicionado para o GitHub reconhecer a pasta
+namespace BillingMVC.Data.Configurations
+{
+    class Config
+    {
+    }
+}
